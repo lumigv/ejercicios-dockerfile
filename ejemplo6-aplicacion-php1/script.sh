cfa0d58b2d310ca69eb7d7db6ba970c4db2baf4e
lumigv <lumigv@gmail.com>
2024-01-22 14:56:18 +0100

    Eliminamos script.sh

--- ejemplo6-aplicacion-php1/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while ! mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST}  -e ";" ; do
-	sleep 1
-done	
-mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST} ${DB_NAME} < /opt/schema.sql
-#apache2ctl -D FOREGROUND
